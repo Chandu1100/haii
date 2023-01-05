6a7591a754afbd648bbb7968018de7d8fcbc757b
Chandu1100 <122019944+Chandu1100@users.noreply.github.com>
2023-01-05 20:08:37 +0530

    Add files via upload

---
+++ haii.cpp
@@ -0,0 +1,13 @@
+#include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"
+MAX30105 particleSensor;
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
+void setup()
+{
+Serial.begin(11520
